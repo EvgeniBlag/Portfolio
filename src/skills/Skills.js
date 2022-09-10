73c6a9b9596f91dc4598c101589d28e940a474d8
EvgeniBlag <blagodarni@gmail.com>
2022-09-10 11:12:36 +0200

    BlockMain

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Skill from './skill/Skill';
 import s from'./Skills.module.css';
 
 
@@ -7,9 +8,13 @@ function Skills() {
     return (
         <div className={s.skillsBlock}>
           <div className={s.skillsContainer}>
+
+
             <h2 className={s.title}>Skills</h2>
             <div className={s.skills}>
-                sdfgh
+               <Skill title={'Js'} description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eaque voluptatum culpa excepturi ad praesentium quia alias magnam '}/>
+               <Skill title={'Css'} description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eaque voluptatum culpa excepturi ad praesentium quia alias magnam '}/>
+               <Skill title={'React'} description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eaque voluptatum culpa excepturi ad praesentium quia alias magnam'}/>
             </div>
           </div>
         </div>
